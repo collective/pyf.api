4aed38b2a8da58493b462480e55225b529657b39
Jens W. Klein <jk@kleinundpartner.at>
2019-02-28 09:49:32 +0100

    first green test

--- src/pyf/api/search.py
+++ src/pyf/api/search.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+from pyf.api.utils import get_search
+
+
+def sort_on_score(search, order):
+    return search.sort({'_score': order})
 
 
 class Search(object):
@@ -6,3 +11,6 @@ class Search(object):
     def __init__(self, settings, params):
         self.settings = settings
         self.params = params
+        self.index = settings['presenter.es.index']
+        self.search = get_search(settings)
+
